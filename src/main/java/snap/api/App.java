2a36db62df76754dd064d6b255a5fb5f9026869a
Yassine AZIMANI <yassine.azimani@hotmail.fr>
2019-07-29 19:34:52 +0200

    first commit

---
+++ src/main/java/snap/api/App.java
@@ -0,0 +1,13 @@
+package snap.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
